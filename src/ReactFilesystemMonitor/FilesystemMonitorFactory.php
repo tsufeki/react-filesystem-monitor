4eeac3944f2403c6d84452618094068a6eab3ac6
tsufeki <tsufeki@ymail.com>
2018-04-26 21:34:23 +0200

    Detect OS in the factory.

--- src/ReactFilesystemMonitor/FilesystemMonitorFactory.php
+++ src/ReactFilesystemMonitor/FilesystemMonitorFactory.php
@@ -6,6 +6,10 @@ class FilesystemMonitorFactory implements FilesystemMonitorFactoryInterface
 {
     public function create(string $path, array $events = null, array $options = [])
     {
-        return new INotifyProcessMonitor($path, $events, $options);
+        if (php_uname('s') === 'Linux') {
+            return new INotifyProcessMonitor($path, $events, $options);
+        }
+
+        return new FsWatchProcessMonitor($path, $events, $options);
     }
 }
